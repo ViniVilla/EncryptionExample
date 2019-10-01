0add13756e4466693f076a11ff964bd752d1b7f0
ViniVilla <viniciuscastilhovilla@gmail.com>
2019-09-30 23:18:26 -0300

    Changes all implementation to jasypt

--- src/main/java/br/edu/ifsp/encryption/demo/controller/CryptoController.java
+++ src/main/java/br/edu/ifsp/encryption/demo/controller/CryptoController.java
@@ -2,6 +2,7 @@ package br.edu.ifsp.encryption.demo.controller;
 
 import br.edu.ifsp.encryption.demo.domain.Mensagem;
 import lombok.extern.slf4j.Slf4j;
+import org.jasypt.util.text.AES256TextEncryptor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,20 +10,12 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import javax.crypto.*;
-import javax.crypto.spec.IvParameterSpec;
-import javax.crypto.spec.PBEKeySpec;
-import javax.crypto.spec.SecretKeySpec;
-import java.security.spec.KeySpec;
-import java.util.Base64;
-
 @RequestMapping("/")
 @Controller
 @Slf4j
 public class CryptoController {
 
     private static String secretKey = "AESEncriptionTest";
-    private static String salt = "AESEncription";
 
     @GetMapping
     public String mostrarPagina(){
@@ -40,31 +33,15 @@ public class CryptoController {
         log.info("Controller encriptar");
         log.info("Mensagem antes de ser encriptada: {}", mensagem.getMensagem());
 
-        try
-        {
-            byte[] iv = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
-            IvParameterSpec ivspec = new IvParameterSpec(iv);
+        AES256TextEncryptor textEncryptor = new AES256TextEncryptor();
+        textEncryptor.setPassword(secretKey);
+        String encriptada = textEncryptor.encrypt(mensagem.getMensagem());
 
-            SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
-            KeySpec spec = new PBEKeySpec(secretKey.toCharArray(), salt.getBytes(), 65536, 256);
-            SecretKey tmp = factory.generateSecret(spec);
-            SecretKeySpec secretKey = new SecretKeySpec(tmp.getEncoded(), "AES");
+        log.info("Mensagem depois de ser encriptada: {}", encriptada);
+        model.addAttribute("encriptar", true);
+        model.addAttribute("mensagem", encriptada);
+        return "sucesso";
 
-            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
-            cipher.init(Cipher.ENCRYPT_MODE, secretKey, ivspec);
-            String encriptada = Base64.getEncoder().encodeToString(cipher.doFinal(mensagem.getMensagem().getBytes("UTF-8")));
-
-            log.info("Mensagem depois de ser encriptada: {}", encriptada);
-            model.addAttribute("encriptar", true);
-            model.addAttribute("mensagem", encriptada);
-            return "sucesso";
-        }
-        catch (Exception e)
-        {
-            log.error("Erro na encriptação");
-        }
-
-        return "home";
     }
 
 
@@ -74,30 +51,13 @@ public class CryptoController {
         log.info("Controller desencriptar");
         log.info("Mensagem antes de ser desencriptada: {}", mensagem.getMensagem());
 
-        try
-        {
-            byte[] iv = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
-            IvParameterSpec ivspec = new IvParameterSpec(iv);
+        AES256TextEncryptor textEncryptor = new AES256TextEncryptor();
+        textEncryptor.setPassword(secretKey);
+        String desencriptada = textEncryptor.decrypt(mensagem.getMensagem());
 
-            SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
-            KeySpec spec = new PBEKeySpec(secretKey.toCharArray(), salt.getBytes(), 65536, 256);
-            SecretKey tmp = factory.generateSecret(spec);
-            SecretKeySpec secretKey = new SecretKeySpec(tmp.getEncoded(), "AES");
-
-            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING");
-            cipher.init(Cipher.DECRYPT_MODE, secretKey, ivspec);
-            String desencriptada = new String(cipher.doFinal(Base64.getDecoder().decode(mensagem.getMensagem())));
-
-            log.info("Mensagem depois de ser desencriptada: {}", desencriptada);
-            model.addAttribute("encriptar", false);
-            model.addAttribute("mensagem", desencriptada);
-            return "sucesso";
-        }
-        catch (Exception e)
-        {
-            log.error("Erro na desencriptação");
-        }
-
-        return "home";
+        log.info("Mensagem depois de ser desencriptada: {}", desencriptada);
+        model.addAttribute("encriptar", false);
+        model.addAttribute("mensagem", desencriptada);
+        return "sucesso";
     }
 }
